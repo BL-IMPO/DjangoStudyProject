f50f1176e1e408b580e376599eda788bed734278
BL-IMPO <nikitka,durne@gmail.com>
2025-03-19 19:30:34 +0600

    Half of the program is ready.

---
+++ base/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_page, name='logout'),
+    path('register/', views.register_page, name='register'),
+
+
+    path('', views.home, name='home'),
+    path('room/<str:pk>/', views.room, name='room'),
+    path('profile/<str:pk>', views.user_profile, name='user-profile'),
+
+    path('create_room/', views.create_room, name='create-room'),
+    path('update_room/<str:pk>/', views.update_room, name='update-room'),
+    path('delete_room/<str:pk>/', views.delete_room, name='delete-room'),
+    path('delete_message/<str:pk>/', views.delete_message, name='delete-message')
+]
