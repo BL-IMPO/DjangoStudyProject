8fae27a534cc4c10acdf77364833a6d7e658f7cf
BL-IMPO <nikitka,durne@gmail.com>
2025-03-25 19:10:52 +0600

    Made mobile phone responsibility for this project.

--- base/views.py
+++ base/views.py
@@ -80,7 +80,7 @@ def home(request):
         Q(description__icontains=q)
     )
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.all().filter(Q(room__topic__name__icontains=q))
 
@@ -200,3 +200,15 @@ def update_user(request):
             return redirect('user-profile', pk=user.id)
 
     return render(request, 'base/update-user.html', {'form': form})
+
+
+def topics_page(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
+
+
+def activity_page(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
